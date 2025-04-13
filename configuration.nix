976fe8eeb18d3e13c11cf90f6818a4063ecfb92f
EsVagy42 <sltamagotchi@gmail.com>
2025-04-13 16:40:50 +0000

    Fixes #6, added retroarch-full instead of retroarch

--- configuration.nix
+++ configuration.nix
@@ -107,7 +107,22 @@
       "steam"
       "steam-original"
       "steam-unwrapped"
+      "libretro-fbalpha2012"
+      "libretro-fbneo"
+      "libretro-fmsx"
       "libretro-genesis-plus-gx"
+      "libretro-mame2000"
+      "libretro-mame2003"
+      "libretro-mame2003-plus"
+      "libretro-mame2010"
+      "libretro-mame2015"
+      "libretro-opera"
+      "libretro-picodrive"
+      "libretro-snes9x"
+      "libretro-snes9x2002"
+      "libretro-snes9x2005"
+      "libretro-snes9x2005-plus"
+      "libretro-snes9x2010"
     ];
 
   services.flatpak.enable = true;
@@ -212,7 +227,7 @@
     mindustry
     openttd-jgrpp
 
-    (retroarch.withCores (cores: with cores; [ genesis-plus-gx ]))
+    retroarch-full
 
     wineWowPackages.stable
     winetricks

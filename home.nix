90d280b5cdce32b5fdfb35045ae9be57cf330238
EsVagy42 <sltamagotchi@gmail.com>
2025-05-29 17:44:39 +0200

    Removed gtk theme from home.nix

--- home.nix
+++ home.nix
@@ -27,14 +27,6 @@
     userEmail = "sltamagotchi@gmail.com";
   };
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Breeze-Dark";
-      package = pkgs.kdePackages.breeze-gtk;
-    };
-  };
-
   home.file.".config/konsolerc" = { source = ./konsole/konsolerc; };
 
   home.file.".local/share/konsole/Konsole.profile" = {

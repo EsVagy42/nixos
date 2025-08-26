206162e5444e5d9cc7d801e99e4302cd2226415e
EsVagy42 <sltamagotchi@gmail.com>
2025-08-26 09:27:15 +0200

    Changed weather widget provider and location to BBC and Budapest

--- plasma-configuration.nix
+++ plasma-configuration.nix
@@ -1440,7 +1440,7 @@
       "plasma-org.kde.plasma.desktop-appletsrc"."Containments/25/Applets/200/Configuration/Units"."visibilityUnit" =
         2007;
       "plasma-org.kde.plasma.desktop-appletsrc"."Containments/25/Applets/200/Configuration/WeatherStation"."source" =
-        "dwd|weather|Miskolc|12772";
+        "bbcukmet|weather|Budapest, Hungary, HU|3054643";
       "plasma-org.kde.plasma.desktop-appletsrc"."Containments/25/Applets/203"."immutability" =
         1;
       "plasma-org.kde.plasma.desktop-appletsrc"."Containments/25/Applets/203"."plugin" =
